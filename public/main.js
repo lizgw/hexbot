1a03207fa0ae4d08882f4596da7abc5fcf3fa580
Liz Wigglesworth <lizwigg6@gmail.com>
2019-06-20 22:20:30 -0500

    Create colored boxes with colors from API

--- public/main.js
+++ public/main.js
@@ -1,11 +1,31 @@
+let playerLevel = 1;
+
 // make a request
-function getColors(numColors) {
+function setupLevel(levelNum) {
+    // figure out how many colors to get
+    let numColors = calculateNumColors(levelNum);
+
     // get colors from the API
     $.get(`https://api.noopschallenge.com/hexbot?count=${numColors}`, function(response) {
-        console.log(response);
+        // build the html level for the colors
+        setupLayout(response.colors);
     });
 }
 
-getColors(1);
-getColors(5);
-getColors(100);
+// calculate the number of colors to use based on the player's current level
+function calculateNumColors(level) {
+    return level * 3;
+}
+
+function setupLayout(colorArray) {
+    // make a color-box for each color
+    for (let color of colorArray) {
+        let box = document.createElement("div");
+        box.classList.add("color-box");
+        box.style.background = color.value;
+        $("#game-canvas").append(box);
+    }
+}
+
+// build a game based on the player's current level
+setupLevel(playerLevel);
